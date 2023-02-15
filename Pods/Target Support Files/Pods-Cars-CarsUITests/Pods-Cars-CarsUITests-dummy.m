422120036691b2456a94741cc2191062a14d89c3
Michael Dugah <michael.dugah@softwaregroup.com>
2023-02-15 11:25:02 +0000

    remove Pods file

--- Pods/Target Support Files/Pods-Cars-CarsUITests/Pods-Cars-CarsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cars_CarsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Cars_CarsUITests
-@end
--- Pods/Target Support Files/Pods-CarsTests/Pods-CarsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CarsTests : NSObject
-@end
-@implementation PodsDummy_Pods_CarsTests
-@end
